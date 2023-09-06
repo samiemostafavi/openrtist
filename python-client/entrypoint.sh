2da5e59a525315957ec9980f9adbf38a06a0d213
Samie Mostafavi <ssmos@kth.se>
2023-09-07 00:32:11 +0200

    add sleep to python docker for expeca

--- python-client/entrypoint.sh
+++ python-client/entrypoint.sh
@@ -18,3 +18,5 @@ SOURCE_NAME = "openrtist"
 EOL
 
 sh -c './src/openrtist/sinfonia_wrapper.py -v ./Big_Buck_Bunny_1080_10s_5MB.mp4 -c $SERVER -o $OUTPUT_ADDR --quiet True -u $DURATION'
+
+sleep infinity
